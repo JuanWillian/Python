a375c26aa9de436ef0d6fda927b15c46aad129a9
Juan Willian <juanwcaraujo28@gmail.com>
2024-09-21 21:23:11 -0300

    Atividade senai

--- SituaçãoDeAprendizagem/Media.py
+++ SituaçãoDeAprendizagem/Media.py
@@ -1,3 +1,7 @@
 vet = [int(input("Número:")) for i in range(20)]
-print("Maior número:", max(vet), "Menor número:", min(vet))
+media =0
+for i in range(20):
+    media +=vet[i]
+media = media/len(vet)
+print("Maior número:", max(vet), "Menor número:", min(vet),"Média:",media)
 
